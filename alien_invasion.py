bc4cada2e1afcdd08d7668b49817b8bc95d85c69
CRUSH007 <sanya.tumanow2011@yandex.ru>
2024-09-08 16:32:09 +0400

    16:31 08.09.2024

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,34 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
+
+    def __init__(self):
+        """Инициализирует игру и создает ресурсы"""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Запускает основной цикл игры"""
+        while True:
+            # отслеживание событий клавиатуры и мыши
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # При каждом прохождении цикла перерисовывается экран.
+            self.screen.fill(self.settings.bg_color)
+
+            # отображение последнего прорисованного экрана.
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # создание экземпляра и запуск игры
+    ai = AlienInvasion()
+    ai.run_game()
